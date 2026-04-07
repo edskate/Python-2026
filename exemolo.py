87b394677ced583bb26f6f1f4643d0869c08fba7
Edy_97_2024 <edskateicevida@gmail.com>
2026-04-07 01:00:21 -0300

    atualização

---
+++ exemolo.py
@@ -0,0 +1,27 @@
+
+
+
+foo = "Hello, World!"
+
+Foo  = 3 + 3
+
+Foo = 3 + 26
+
+print(foo)
+print(Foo)
+
+we = "Welcome to Python programming."
+print(we)
+
+def greet():
+    print("This is a simple Python script.")
+greet()
+print("Python is a versatile language.")
+message = "You can use it for web development, data analysis, and more."
+print(message)
+def learn():
+    print("Let's learn Python together!")
+learn()
+
+
+
