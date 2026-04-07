8f69c336a97975c1ae70097365a0e5e790634530
Edy_97_2024 <edskateicevida@gmail.com>
2026-04-07 00:27:53 -0300

    01.py

---
+++ 01.py
@@ -0,0 +1,6 @@
+print("Hello, World!");
+print("Welcome to Python programming.");
+print("This is a simple Python script.");
+print("Python is a versatile language.");
+print("You can use it for web development, data analysis, and more.");
+print("Let's learn Python together!");
