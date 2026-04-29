7e72ea62d7b1c6112fd9ba1df08cc16e7b09c35c
Edy_97_2024 <edskateicevida@gmail.com>
2026-04-29 13:38:13 -0300

    lista e pre;os

---
+++ 04.py
@@ -0,0 +1,9 @@
+listaprodutos = ["Produto 1", "Produto 2", "Produto 3"]
+print("Lista de Produtos:")
+for produto in listaprodutos:
+    print(produto)
+print("\nAdicionando um novo produto...")
+listaprodutos.append("Produto 4")
+print("Lista de Produtos atualizada:")
+for produto in listaprodutos:
+    print(produto)
---
+++ 05.py
@@ -0,0 +1,4 @@
+listaprodutos = ["iphone", "samsung", "xiaomi"]// O código acima define uma lista de produtos com três itens: "iphone", "samsung" e "xiaomi".
+listaprecos = [1500, 50000, 1000]// O código acima define uma lista de produtos e uma lista de preços correspondentes.
+for item in listaprecos:// O código acima irá iterar sobre cada preço na lista listaprecos.
+    print(item * 2)// O código acima irá imprimir o dobro de cada preço na lista listaprecos.
