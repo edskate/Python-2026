c14e40181594d0b41ff432ee8e86111daffae650
Edy_97_2024 <edskateicevida@gmail.com>
2026-05-03 11:03:36 -0300

    create 07.py

---
+++ 07.py
@@ -0,0 +1,4 @@
+print("Ola mundo, este é o meu primeiro programa em Python!")
+print("Estou aprendendo a programar e estou muito animado!")
+print("Python é uma linguagem de programação muito popular e fácil de aprender.")
+print("Com Python, posso criar aplicativos, jogos, sites e muito mais!")
