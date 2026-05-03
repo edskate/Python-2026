bc443ed1adf2aea2cef76f3c1c3565a61472dc92
Edy_97_2024 <edskateicevida@gmail.com>
2026-05-03 12:12:39 -0300

    ok

---
+++ 14.py
@@ -0,0 +1,8 @@
+# le uma string 
+
+nome = input("Digite seu nome: ")
+# imprima a string 
+print("Olá, " + nome + "!")
+
+idade = int(input("Digite sua idade: "))
+print(f"Seu nome é {nome} e sua idade é {idade}")
